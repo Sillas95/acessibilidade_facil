62dc8ce20624ee2198e835debbb9fcb7db6d082c
Sillas95 <sillas123silva@gmail.com>
2023-08-15 17:33:16 -0300

    Update myhomepage.dart

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'asd',
+                  labelText: 'Usuário',
                 ),
               ),
             ),
